681fe332cee87a24bd1d2403d273cfaec3d3abc1
tiacrane <96049871+tiacrane@users.noreply.github.com>
2023-10-25 19:48:09 +0200

    Update Tia_summieren.cc
    tested summation file, will upload txt

--- Tia_summieren.cc
+++ Tia_summieren.cc
@@ -1,5 +1,35 @@
-void Tia_summieren() {
+#include <fstream>
+#include <iostream>
+
+int main(){
+  // Exercise 01:
+
+  int data_val, prev_data_val;
+  int pair_sum;
+  
   std::ifstream fin("daten.txt");
-  for(int i; i < 2, ++i)
+  std::ofstream fout("datensumme.txt");
+  
+  if ( fin.is_open() ){
+    int j = 0, w;
+    for(int i; i<234; ++i){
+      fin >> data_val;
+      if(i%2 == 0){
+        j+=1, w=0; 
+        prev_data_val=data_val;
+      } //{std::cout << "index: " << i << std::endl;}
+      else{
+        w=1; 
+        pair_sum = data_val+prev_data_val;
+      }
+      std::cout << "(row,index): (" << j << ", " << w << "), value: " << data_val << std::endl;
+      if(i%2 != 0){
+        std::cout << ">> row summation = " << pair_sum << std::endl << std::endl;
+        fout << pair_sum << std::endl;
+      }
+    }
   }
+  fin.close(); fout.close();
+
+  std::cout << "Done!" << std::endl;
 }
