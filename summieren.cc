461819338f841bae1953be1ed9fed893dbc9e8f7
ShiroeCake <148950189+ShiroeCake@users.noreply.github.com>
2023-11-01 14:56:39 +0000

    Summieren erzeugt

---
+++ summieren.cc
@@ -0,0 +1,52 @@
+#include <fstream>
+#include <iostream>
+
+int main(){
+  int oddNumber;
+  int evenNumber;
+  int sumNumber;
+  std::ifstream fin("daten.txt");
+  std::ofstream fout("datensumme.txt");
+  for(int i; i<468; ++i){
+      fin >> evenNumber;
+      fin >> oddNumber;
+      sumNumber = evenNumber + oddNumber;
+      fout << sumNumber << std::endl;
+  }
+  fin.close();
+  fout.close();
+  std::cout << "Done" << std::endl;
+}
+
+/*int main(){
+  // Exercise 01:
+
+  int data_val, prev_data_val;
+  int pair_sum;
+  
+  std::ifstream fin("daten.txt");
+  std::ofstream fout("datensumme.txt");
+  
+  if ( fin.is_open() ){
+    int j = 0, w;
+    for(int i; i<234; ++i){
+      fin >> data_val;
+      if(i%2 == 0){
+        j+=1, w=0; 
+        prev_data_val=data_val;
+      } //{std::cout << "index: " << i << std::endl;}
+      else{
+        w=1; 
+        pair_sum = data_val+prev_data_val;
+      }
+      std::cout << "(row,index): (" << j << ", " << w << "), value: " << data_val << std::endl;
+      if(i%2 != 0){
+        std::cout << ">> row summation = " << pair_sum << std::endl << std::endl;
+        fout << pair_sum << std::endl;
+      }
+    }
+  }
+  fin.close(); fout.close();
+
+  std::cout << "Done!" << std::endl;
+}*/
